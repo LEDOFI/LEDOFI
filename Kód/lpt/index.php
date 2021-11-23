6757efe3eea1d4c61b220ae351ad661e164b402f
musil32 <93339698+musil32@users.noreply.github.com>
2021-11-23 20:47:56 +0100

    Čtenář: Volný přístup k obsahu časopisu
    Čtenář může vidět všechny vydané články na hlavní stránce bez nutnosti se přihlásit/zaregistrovat.

--- Kód/lpt/index.php
+++ Kód/lpt/index.php
@@ -1,20 +1,31 @@
-<?php
+<?php 
 include("path.php");
-include(ROOT_PATH . "/app/database/db.php");
+session_start();
+require(ROOT_PATH . "/app/database/db.php");
+include(ROOT_PATH . '/app/controllers/doc2txt.class.php');
+
+$posts = array();
+$postsTitle = 'Recent Posts';
+
 ?>
 <!DOCTYPE html>
-<html lang="cs">
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=BioRhyme&family=Lora&display=swap" rel="stylesheet">
 
+  <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BioRhyme&family=Lora&display=swap" rel="stylesheet">
+
+  <!-- Custom Styling -->
   <link rel="stylesheet" href="assets/css/style.css">
 
   <title>Los Polos Technikos</title>
@@ -22,14 +33,15 @@ include(ROOT_PATH . "/app/database/db.php");
 
 <body>
 
-<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+  <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
 
 
-<?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
 
-
+  <!-- Page Wrapper -->
   <div class="page-wrapper">
 
+    <!-- Post Slider -->
     <div class="post-slider">
       <h1 class="slider-title">Oblíbené články</h1>
       <i class="fas fa-chevron-left prev"></i>
@@ -37,158 +49,100 @@ include(ROOT_PATH . "/app/database/db.php");
 
       <div class="post-wrapper">
 
-        <div class="post">
-          <img src="images/img1.jpg" alt="" class="slider-image">
-          <div class="post-info">
-            <h4><a href="#">Lorem ipsum dolor sit amet</a></h4>
-            <i class="far fa-user">Zdeněk Šrámek</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
+		<?php
+		
+			if (isset($_POST['search-term'])) {
+				$postsTitle = "You searched for '" . $_POST['search-term'] . "'";
+				$posts = $_POST['search-term'];
+				$match = '%' . $posts . '%';
+				$sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=1 AND p.title LIKE '$match'";
+			} else {
+				$sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=1";
+				
+			}
+				$results = mysqli_query($conn,$sql);
+				while ($post = mysqli_fetch_assoc($results)) {
+		?>
+          <div class="post">
+            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="slider-image">
+            <div class="post-info">
+              <h4><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h4>
+              <i class="far fa-user"> <?php echo $post['autor_clanku']; ?></i>
+              &nbsp;
+              <i class="far fa-calendar"> <?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+            </div>
           </div>
-        </div>
-
-        <div class="post">
-          <img src="assets/images/img1.jpg" alt="" class="slider-image">
-          <div class="post-info">
-            <h4><a href="#">Lorem ipsum dolor sit amet</a></h4>
-            <i class="far fa-user">Aleš Brabec</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-          </div>
-        </div>
-
-        <div class="post">
-          <img src="assets/images/img1.jpg" alt="" class="slider-image">
-          <div class="post-info">
-            <h4><a href="#">Lorem ipsum dolor sit amet</a></h4>
-            <i class="far fa-user">Tadeáš Fejt</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-          </div>
-        </div>
-
-        <div class="post">
-          <img src="assets/images/img1.jpg" alt="" class="slider-image">
-          <div class="post-info">
-            <h4><a href="#">Lorem ipsum dolor sit amet</a></h4>
-            <i class="far fa-user">Jan Musil</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-          </div>
-        </div>
-
-        <div class="post">
-          <img src="assets/images/img1.jpg" alt="" class="slider-image">
-          <div class="post-info">
-            <h4><a href="#">Lorem ipsum dolor sit amet</a></h4>
-            <i class="far fa-user">Unknown</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-          </div>
-        </div>
+		<?php } ?>
 
 
       </div>
 
     </div>
+    <!-- // Post Slider -->
 
+    <!-- Content -->
     <div class="content clearfix">
 
+      <!-- Main Content -->
       <div class="main-content">
-        <h1 class="recent-post-title">Nedávno přidané</h1>
+        <h1 class="recent-post-title"><?php echo $postsTitle ?></h1>
 
-        <div class="post clearfix">
-          <img src="assets/images/img1.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h2><a href="#">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a></h2>
-            <i class="far fa-user">Unknown</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-            <p class="preview-text">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Exercitationem optio possimus a inventore maxime laborum.
-            </p>
-            <a href="#" class="btn read-more">Zobrazit více</a>
-          </div>
-        </div>
+		<?php
+				$results = mysqli_query($conn,$sql);
+				while ($post = mysqli_fetch_assoc($results)) {
+		?>
+          <div class="post clearfix">
+            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="post-image">
+            <div class="post-preview">
+              <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+              <i class="far fa-user"> <?php echo $post['autor_clanku']; ?></i>
+              &nbsp;
+              <i class="far fa-calendar"> <?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+              <p class="preview-text">
+			  	<?php
+				$docObj = new Doc2Txt(ROOT_PATH . "/assets/documents/" . $post['document']);
 
-        <div class="post clearfix">
-          <img src="assets/images/img1.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h2><a href="#">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a></h2>
-            <i class="far fa-user">Unknown</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-            <p class="preview-text">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Exercitationem optio possimus a inventore maxime laborum.
-            </p>
-            <a href="#" class="btn read-more">Zobrazit více</a>
-          </div>
-        </div>
-        <div class="post clearfix">
-          <img src="assets/images/img1.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h2><a href="#">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a></h2>
-            <i class="far fa-user">Unknown</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-            <p class="preview-text">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Exercitationem optio possimus a inventore maxime laborum.
-            </p>
-            <a href="#" class="btn read-more">Zobrazit více</a>
-          </div>
-        </div>
-        <div class="post clearfix">
-          <img src="assets/images/img1.jpg" alt="" class="post-image">
-          <div class="post-preview">
-            <h2><a href="#">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a></h2>
-            <i class="far fa-user">Unknown</i>
-            &nbsp;
-            <i class="far fa-calendar">10. listopadu 2021</i>
-            <p class="preview-text">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Exercitationem optio possimus a inventore maxime laborum.
-            </p>
-            <a href="#" class="btn read-more">Zobrazit více</a>
-          </div>
-        </div>
+				$txt = $docObj->convertToText();
+				echo substr($txt, 0, 150);
+				?>
+              </p>
+              <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
+            </div>
+          </div>    
+		<?php } ?>
+        
+
 
       </div>
+      <!-- // Main Content -->
 
       <div class="sidebar">
 
         <div class="section search">
           <h2 class="section-title">Vyhledávání</h2>
           <form action="index.php" method="post">
-            <input type="text" name="search-term" class="text-input" placeholder="Hledej...">
+            <input type="text" name="search-term" class="text-input" placeholder="Search...">
           </form>
-        </div>
-
-
-        <div class="section topics">
-          <h2 class="section-title">Témata</h2>
-          <ul>
-            <li><a href="#">Programování</a></li>
-            <li><a href="#">Finance</a></li>
-            <li><a href="#">Matematika</a></li>
-            <li><a href="#">Webové stránky</a></li>
-          </ul>
         </div>
 
       </div>
 
     </div>
+    <!-- // Content -->
 
   </div>
+  <!-- // Page Wrapper -->
 
   <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
+
+  <!-- JQuery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+  <!-- Slick Carousel -->
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
+  <!-- Custom Script -->
   <script src="assets/js/scripts.js"></script>
 
 </body>
