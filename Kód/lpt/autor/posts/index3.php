aa552c8c9a51bc8c230f8216279c135bdab38339
AlesBrabec <92459901+AlesBrabec@users.noreply.github.com>
2021-12-09 15:32:09 +0100

    Update index3.php

--- Kód/lpt/autor/posts/index3.php
+++ Kód/lpt/autor/posts/index3.php
@@ -11,7 +11,7 @@ $autors = array();
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>bo
+    <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
